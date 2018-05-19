62f5272881fe8d58aed6a31884a28f85ee82e171
Vítězslav Slavík <vitezslavik@gmail.com>
2018-05-19 10:58:54 +0200

    updating index page style + content

--- app/index.php
+++ app/index.php
@@ -37,32 +37,76 @@
     </div>
   </header>
   <main>
-    <section id="section-icons" class="container bg-light">
+    <section id="section-icons" class="container bg-light h-100">
       <div class="row">
-        <div class="col-12 col-md-4 d-inline-flex d-md-flex flex-md-column align-items-center px-3 py-4"><i class="pr-3 mx-auto md-dark material-icons md-front">work</i>
+        <div class="section-item col-12 col-md-4 d-inline-flex d-md-flex flex-md-column align-items-center justify-content-center">
+          <i class="mx-auto md-dark material-icons mi-home">highlight</i>
           <p>Create a project for your goals, work stuff or for every role of your. It is up to you!</p>
         </div>
-        <div class="col-12 col-md-4 d-inline-flex d-md-flex flex-md-column align-items-center px-3 py-4"><i class="pr-3 mx-auto md-dark material-icons md-front">list</i>
-          <p>Make list of working tasks or just a checklist for your next party. What is written, can't be forgotten.</p>
+        <div class="section-item col-12 col-md-4 d-inline-flex d-md-flex flex-md-column align-items-center justify-content-center">
+          <i class="mx-auto md-dark material-icons mi-home">done_all</i>
+          <p>Make list of tasks or a checklist for your next party. What is written, can't be forgotten.</p>
         </div>
-        <div class="col-12 col-md-4 d-inline-flex d-md-flex flex-md-column align-items-center px-3 py-4"><i class="pr-3 mx-auto md-dark material-icons md-front">label</i>
+        <div class="section-item col-12 col-md-4 d-inline-flex d-md-flex flex-md-column align-items-center justify-content-center">
+          <i class="mx-auto md-dark material-icons mi-home">label</i>
           <p>Label the task with the context. Prioritize them with Eisenhower matrix. Know what matters.</p>
         </div>
       </div>
     </section>
-    <section id="third-section" class="container-fluid">
-      <div class="row h-100 justify-content-center overlay-background">
-        <div class="col-12 h-100 d-flex flex-column justify-content-center font-light">
-          <h2 class="mx-auto">Have a control of your life!</h2>
-          <p class="mx-auto">Life is a game and world is a playground. Make sure you play it with right tools.</p>
+    <article class="quote-overlay">
+      <section class="container-fluid quote-section">
+        <div class="row h-100 justify-content-center overlay-background">
+          <div class="col-12 h-100 d-flex flex-column justify-content-center font-light">
+            <h2 class="mx-auto">Have a control of your life!</h2>
+            <p class="mx-auto">Life is a game and world is a playground. Make sure you play it with right tools.</p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section class="container-fluid bg-light quote-section">
+        <div class="row h-100 justify-content-center">
+          <div class="col-12 h-100 d-flex flex-column justify-content-center align-content-center">
+            <div class="h-box d-flex justify-content-center align-content-center">
+              <i class="material-icons mi-quote">school</i>
+              <h2 class="">School project!</h2>
+            </div>
+            <p>App was created for school purposes on 2iz278 - Web applications</p>
+          </div>
+        </div>
+      </section>
+      
+      <section class="container-fluid quote-section">
+        <div class="row h-100 justify-content-center overlay-background">
+          <div class="col-12 h-100 d-flex flex-column justify-content-center font-light">
+            <h2 class="pb-3 text-center">Whole app on the Github!</h2>
+            <div class="container">
+              <div class="row align-self-center justify-content-center github-box">
+                <div class="col-2 d-flex align-items-center justify-content-center">
+                  <!-- Place this tag where you want the button to render. -->
+                  <!-- Place this tag where you want the button to render. -->
+                  <a class="github-button mx-auto" href="https://github.com/NightingaleV" data-size="large" aria-label="Follow @NightingaleV on GitHub">Follow @NightingaleV</a>
+                </div>
+                <div class="col-2 d-flex align-items-center justify-content-center">
+                  <!-- Place this tag where you want the button to render. -->
+                  <a class="github-button" href="https://github.com/ntkme/github-buttons/issues" data-icon="octicon-issue-opened" data-size="large" aria-label="Issue ntkme/github-buttons on GitHub">Report an Issue!</a>
+                </div>
+                <div class="col-2 d-flex align-items-center justify-content-center">
+                  <!-- Place this tag where you want the button to render. -->
+                  <a class="github-button mx-auto" href="https://github.com/ntkme/github-buttons/archive/master.zip" data-icon="octicon-cloud-download" data-size="large" aria-label="Download ntkme/github-buttons on GitHub">Download the project here!</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
   </main>
+
 </body>
 <!--Bootstrap JS-->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
 <script src="js/main.js"></script>
 </html>
