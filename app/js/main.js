acde1b10ec114d9b4e71e7da1548a26fce6564f3
Vítězslav Slavík <vitezslavik@gmail.com>
2018-06-03 21:16:14 +0200

    add create task from inline functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
+
 //CREATE TASK THROUGHT INLINE TAB
 //auto focus input for task
 $('#addTaskInline').on('shown.bs.collapse', function (e) {
-  
+  $('.addTaskInline-input').val('');
   $('.addTaskInline-input').focus();
+});
+//Inline form - click add button
+$(document).on('click', '.addTaskInline-btn', function (e) {
+  createTask('inline');
+});
+//Inline form - on enter
+$('.addTaskInline-form').on('submit', function (e) {
+  createTask('inline');
 });
 
 
@@ -37,59 +46,16 @@ $('#addTask').on('shown.bs.modal', function (e) {
 
 });
 
-//Ajax for creating new task
-function createTask() {
-  event.preventDefault();
-
-  if ($('#taskName').val()) {
-    var formData = $('.addTask-form').serialize();
-    console.log(formData);
-    //Grab the current project to rerender
-    var currentProject = $('.addTask-form').find('#projectSelect option:selected').text();
-
-    $.post({
-      url: 'php/create-modules/create-task.php',
-      data: formData,
-      datatype: FormData,
-      success: function (response) {
-        console.log('Success to contact the server');
-        console.log(response);
-        if (response === 'task_added') {
-
-        }
-        if (response === 'duplicated_tasks') {
-          console.log('it isnot');
-          $('#taskName').popover({
-            trigger: 'manual',
-            delay: {
-              "show": 250,
-              "hide": 0
-            }
-          });
-          $('#taskName').popover('toggle');
-          setTimeout(function () {
-            $('#taskName').popover('toggle');
-          }, 4500);
-
-        }
-      },
-      error: function () {
-        console.log('Failure');
-      }
-    });
-  }
-}
-
 //Modal - click add button
 $(document).on('click', '.btn-addTask', function (e) {
-  createTask();
+  createTask('modal');
 });
 //Modal - on enter
 $('.addTask-form').on('submit', function (e) {
-  createTask();
+  createTask('modal');
 });
 
-//Task validation
+//Modal - Task validation
 $('#taskName').on('keyup blur', function () {
   if (!$(this).val()) {
     $(this).siblings('.invalid-tooltip').addClass('d-block');
@@ -97,6 +63,87 @@ $('#taskName').on('keyup blur', function () {
     $(this).siblings('.invalid-tooltip').removeClass('d-block').addClass('d-none');
   }
 });
+
+//Ajax for creating new task
+function createTask(sourceOfAction) {
+  event.preventDefault();
+  
+  //Request is coming from modal or inline input
+  if (sourceOfAction === 'modal') {
+    if ($('#taskName').val()) {
+      var formData = $('.addTask-form').serialize();
+      console.log(formData);
+      
+      //Grab the current project to rerender
+      var currentProject = $('.addTask-form').find('#projectSelect option:selected').text();
+
+      $.post({
+        url: 'php/create-modules/create-task.php',
+        data: formData,
+        datatype: FormData,
+        success: function (response) {
+          console.log('Success to contact the server');
+          console.log(response);
+          if (response === 'task_added') {
+            //succesfull add
+            renderTasks(currentProject);
+          }
+          if (response === 'duplicated_tasks') {
+            console.log('it isnot');
+            $('#taskName').popover({
+              trigger: 'manual',
+              delay: {
+                "show": 250,
+                "hide": 0
+              }
+            });
+            $('#taskName').popover('toggle');
+            setTimeout(function () {
+              $('#taskName').popover('toggle');
+            }, 4500);
+
+          }
+        },
+        error: function () {
+          console.log('Failure');
+        }
+      });
+    }
+  } else if (sourceOfAction === 'inline') {
+    if($('.addTaskInline-input').val()){
+      //Grab the form data
+      var formInlineData = $('.addTaskInline-form').serialize();
+      console.log(formInlineData);
+      console.log('Inline executed');
+      
+      //Grab the current project to rerender
+      var currentProject = $('.project-title').text();
+      
+      //Post the inline form
+      $.post({
+        url: 'php/create-modules/create-task.php',
+        data: formInlineData,
+        datatype: FormData,
+        success: function (response) {
+          console.log('Success to contact the server');
+          console.log(response);
+          if (response === 'task_added') {
+            //succesfull add
+            renderTasks(currentProject);
+            $('#addTaskInline').removeClass('show');
+          }
+          if (response === 'duplicated_tasks') {
+            console.log('it isnot');
+          }
+        },
+        error: function () {
+          console.log('Failure');
+        }
+      });
+    }
+  }
+}
+
 //Modal - click on button will request the inserting the task
 $(document).on('click', '.btn-task-delete',function(e){
   event.preventDefault();
--- app/templates/app-modules/todo-add-task.php
+++ app/templates/app-modules/todo-add-task.php
@@ -5,21 +5,21 @@
 
 <div class="collapse multi-collapse" id="addTaskInline">
   <div class="card card-body">
-    <form class="addTaskInline-form">
+    <form method="post" class="addTaskInline-form" autocomplete="off">
       <div class="addTaskInline-container input-group row">
         <div class="addTaskInline-btn-box col-1 input-group-prepend">
-          <button class="addTaskInline-btn  btn btn-danger" type="button">
-            <i class="material-icons">add</i>
+          <button class="addTaskInline-btn  btn btn-danger" type="submit">
+            <i class="addTaskInline-icon material-icons">add</i>
           </button>
         </div>
-        <input type="text" class="addTaskInline-input form-control col-8" placeholder="My new task is ..." aria-label="New task" aria-describedby="basic-addon2">
-        <select class="addTaskInline-priority form-control input-group-append col-3" data-show-content="true" id="prioritySelect">
+        <input name="task" type="text" class="addTaskInline-input form-control col-8" placeholder="My new task is ..." aria-label="New task" aria-describedby="basic-addon2">
+        <select name="priority" class="addTaskInline-priority form-control input-group-append col-3" data-show-content="true" id="prioritySelect">
           <option value="1">Priority 1</option>
           <option value="2">Priority 2</option>
           <option value="3">Priority 3</option>
           <option value="4" selected>Priority 4</option>
         </select>
-        <input type="hidden" class="addTaskInline-project" name="currentproject" value="<?php echo $_GET['project'] ?>">
+        <input name="project" type="hidden" class="addTaskInline-project" name="currentproject" value="<?php echo $_GET['project'] ?>">
       </div>
     </form>
   </div>
