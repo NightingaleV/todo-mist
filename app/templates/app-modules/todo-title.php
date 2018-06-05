92288fb2bd46913e33d8ff87bd20570caf2791ea
Vítězslav Slavík <vitezslavik@gmail.com>
2018-06-05 18:12:30 +0200

    scss styles for app buttons

--- app/templates/app-modules/todo-title.php
+++ app/templates/app-modules/todo-title.php
@@ -2,10 +2,10 @@
   <h2 class="todo-title"><?php if(isset($_GET['project'])){echo $_GET['project'];}?></h2>
   <div class="top-right-controls">
     <div class="todo-edit">
-      <button class="btn todo-edit-btn" type="button"><i class="project-edit-icon material-icons">edit</i></button>
+      <button class="btn btn-todo-edit" type="button"><i class="edit-icon material-icons">edit</i></button>
     </div>
     <div class="todo-sort">
-      <button class="btn todo-sort-button" type="button"><i class="todo-sort-icon material-icons">sort</i></button>
+      <button class="btn btn-todo-sort" type="button"><i class="sort-icon material-icons">sort</i></button>
     </div>
   </div>
 </div>
