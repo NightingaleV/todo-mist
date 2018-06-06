5592ae33eb405fa545c3571ed06adfa63ce4dba3
Vítězslav Slavík <vitezslavik@gmail.com>
2018-06-06 19:37:23 +0200

    update scss styles

---
+++ app/js/modules/update-priority.js
@@ -0,0 +1,99 @@
+//Change priority
+function changePriority(taskToUpdate,priority,tasksProjectId){
+  
+  var currentFilter = $('.todo-title').text().trim();
+  
+  //Cut the URL into array
+  var urlArray = window.location.href.split('/');
+  //If current filter is tag
+  if(urlArray[3].indexOf('tag') >= 0){
+    console.log('Current tag:'+currentFilter);
+    console.log('Task:'+taskToUpdate);
+    console.log('New priority:'+priority);  
+    $.post({
+        url: 'php/update-modules/update-task-priority.php',
+        data: { currentTag:currentFilter,
+                tasksProject:tasksProjectId,
+                task:taskToUpdate,
+                priority:priority,
+                },
+        success: function (response) {
+          console.log('Success to contact the server');
+          console.log(response);
+        },
+        error: function () {
+          console.log('Fail to connect the server');
+        }
+    });
+  }
+  //If current filter is project
+  else{
+
+    console.log('Current project:'+currentFilter);
+    console.log('Task:'+taskToUpdate);
+    console.log('New priority:'+priority);
+    
+    $.post({
+        url: 'php/update-modules/update-task-priority.php',
+        data: { currentProject:currentFilter,
+                task:taskToUpdate,
+                priority:priority,
+                },
+        success: function (response) {
+          console.log('Success to contact the server');
+          console.log(response);
+        },
+        error: function () {
+          console.log('Fail to connect the server');
+        }
+    });
+  }
+}
+
+//Change class of item 
+function changePriorityClass(changedItem,priority) {
+  if(changedItem.hasClass('priority-1')){   
+    changedItem.removeClass('priority-1'); 
+    changedItem.addClass('priority-'+priority);
+  }
+  else if (changedItem.hasClass('priority-2')){
+    changedItem.removeClass('priority-2'); 
+    changedItem.addClass('priority-'+priority);
+  }
+  else if (changedItem.hasClass('priority-3')){
+    changedItem.removeClass('priority-3'); 
+    changedItem.addClass('priority-'+priority);
+  }
+  else{
+    changedItem.removeClass('priority-4'); 
+    changedItem.addClass('priority-'+priority);
+  }
+}
+//CLick on button change priority
+$(document).on('click', '.priority-dropdown-item',function(e){
+  event.preventDefault();
+  var changedItem = $(this).parents('.todo-item');
+  var taskToUpdate = changedItem.find('.todo-label').text();
+  var tasksProjectId = changedItem.attr('data-project');
+  console.log(taskToUpdate);
+  var priority = 1;
+  if($(this).hasClass('btn-priority-2')){
+    priority = 2;
+  }
+  else if($(this).hasClass('btn-priority-3')){
+    priority = 3;
+  }
+  else if($(this).hasClass('btn-priority-4')){
+    priority = 4;
+    priority = 4;
+  }
+  else{
+  console.log(priority); console.log('No');
+  }
+  //CALL AJAX
+  changePriority(taskToUpdate,priority,tasksProjectId);
+  //Change class and coloring for task
+  changePriorityClass(changedItem,priority);
+  changedItem.attr('data-task-priority', priority);
+  
+});
